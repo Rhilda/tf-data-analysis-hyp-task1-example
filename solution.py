89b624bbbd47dfb9221ffd0e7f85346e6c439b75
Rhilda <ildariismagilov@mail.ru>
2023-04-09 12:54:20 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -9,6 +9,12 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    alpha = 0.05
-    p = stats.mannwhitneyu(x, y).pvalue
-    return p < alpha
+    alpha = 0.1
+    p_y = y_success / y_cnt
+    p_x = x_success / x_cnt
+    p = (y_success + x_success) / (y_cnt + x_cnt)
+    std = np.sqrt(p * (1 - p) / y_cnt + p * (1 - p) / x_cnt)
+    z = (p_y - p_x) / std
+    z_crit = norm.ppf(1 - alpha)
+    answer = True if z > z_crit else False
+    return answer
