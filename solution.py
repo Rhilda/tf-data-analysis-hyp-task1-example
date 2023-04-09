1e7581eb004818d0f8d257079c770704e600efab
Rhilda <ildariismagilov@mail.ru>
2023-04-09 12:41:19 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import mannwhitneyu
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 773978697 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.05
+    p = stats.mannwhitneyu(x, y).pvalue
+    return p < alpha
