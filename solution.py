1d309047b22f53f77df34b901bf5ea5c2bf8b1ac
Rhilda <ildariismagilov@mail.ru>
2023-04-09 13:06:45 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import mannwhitneyu
-
+from statsmodels.stats.proportion import proportions_ztest
 
 chat_id = 773978697 # Ваш chat ID, не меняйте название переменной
 
@@ -9,12 +8,9 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
+    counts = np.array([x_success, y_success])
+    nobs = np.array([x_cnt, y_cnt])
+    
+    z_stat, p_value = proportions_ztest(counts, nobs, alternative = 'larger')
     alpha = 0.1
-    p_y = y_success / y_cnt
-    p_x = x_success / x_cnt
-    p = (y_success + x_success) / (y_cnt + x_cnt)
-    std = np.sqrt(p * (1 - p) / y_cnt + p * (1 - p) / x_cnt)
-    z = (p_y - p_x) / std
-    z_crit = norm.ppf(1 - alpha)
-    answer = True if z > z_crit else False
-    return answer
+    return p_value < alpha
